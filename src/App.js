dc13a6a36ad1397a10fbbfaa82297ae6c2078106
ZOUBAIRI-Y <zobairiyassin@gmail.com>
2024-02-02 18:18:54 +0100

    commit the tvshows tp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import TvShows from './components/TvShows';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TvshowDetails from './components/TvshowDetails';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tvshows, setTVshows] = useState([]);
+  const [search, setSearch] = useState({
+    title: "",
+    type: "",
+    year: 0
+  });
+  const [error, setError] = useState(null)
+  const [id, setId] = useState(null)
+  const fetchTVshows = async (e) => {
+    e.preventDefault()
+    const handleyear = () => {
+      if (search.year) {
+        return search.year
+      }
+    }
+    console.log(search)
+    const resp = await fetch(`https://www.omdbapi.com/?apikey=35b75175&s=${search.title}&type=${search.type}&y=${handleyear()}`)
+    .then(resp=> {
+      if (!resp.ok) {
+        throw Error('Faild to fetch data')
+      }
+      return resp.json()
+    })
+    // .then(resp=> console.log(typeof(resp.Response)))
+    .then(resp=> {
+      if (resp.Response === "False") {
+        throw Error(resp.Error)
+      }
+      setTVshows(resp.Search)
+    })
+    .catch(err=> {
+      alert(err.message)
+    })
+  }
+  
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TvShows fetchTVshows={fetchTVshows} search={search} setSearch={setSearch} tvshows={tvshows} error={error} setId={setId} />} />
+        <Route path='/:tvshow/detail' element={<TvshowDetails id={id} />} />
+      </Routes>
+    </BrowserRouter>
+    {/* {console.log(tvshows)} */}
+  </>
 }
 
 export default App;
